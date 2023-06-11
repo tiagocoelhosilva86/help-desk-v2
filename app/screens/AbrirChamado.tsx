82332ef76d2ac4b77ba0ae2a0bf6cf9f2ad6ecf9
tiagocoelhosilva86 <tiagospaik63@gmail.com>
2023-06-11 13:09:41 -0300

    atualização

--- app/screens/AbrirChamado.tsx
+++ app/screens/AbrirChamado.tsx
@@ -76,7 +76,7 @@ const AbrirChamado = () => {
     setLoading(true);
     let url;
     try {
-      const storageRef = ref(getStorage(), `Chamados/image-${Date.now}`);
+      const storageRef = ref(getStorage(), `Chamados/image-${Date.now()}`);
       await uploadBytes(storageRef, imagemChamado);
       url = await getDownloadURL(storageRef);
     } catch (error) {
@@ -91,7 +91,6 @@ const AbrirChamado = () => {
     })
       .then((dados) => {
         console.log(dados.id);
-
         setLoading(false);
         setModalVisible(true);
         console.log("documento enviado");
